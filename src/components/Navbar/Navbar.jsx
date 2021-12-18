47850ffd91a4b7a29063de9e24b2694162360b19
[Lisa Miner] <midnightmoet@gmail.com>
2021-12-18 14:36:32 -0500

    added buttons with CSS, haven't added functionality YET

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,16 @@
+// import {useContext } from 'react';
+// import { Link } from 'react-router-dom';
 import './navbar.css';
 
 export default function Navbar() {
     return (
         <div className="navbar">
-            Pokemon API
+            Pokemon API  
+            <div className="navbarLogin">
+                <button className="navbarLoginBtn">Login</button>
+                <button className="navbarRegisterBtn">Register</button>
+            </div>          
         </div>
+        
     );
 }
